1f36781d561fa2099e1efec067f878c7940c3850
Will Chen <347109078@qq.com>
2016-08-21 21:50:33 +0800

    Traditional Thread

--- Thread/src/main/java/will/study/thread/traditional/ThreadCommunication.java
+++ Thread/src/main/java/will/study/thread/traditional/ThreadCommunication.java
@@ -6,27 +6,57 @@ package will.study.thread.traditional;
 public class ThreadCommunication {
 
     public static void main(String[] args) {
+
+        final Business business = new Business();
+
         new Thread(new Runnable() {
             public void run() {
                 for (int i = 0; i < 50; i++) {
-
-                    synchronized(ThreadCommunication.class){
-                        for (int j = 0; j < 10; j++) {
-                            System.out.println("sub thread sequece of " + j + " loop of " + i);
-                        }
-                    }
-
+                    business.sub(i);
                 }
             }
-        });
+        }).start();
 
         for (int i = 0; i < 50; i++) {
-            synchronized(ThreadCommunication.class){
-                for (int j = 0; j < 10; j++) {
-                    System.out.println("main thread sequece of " + j + " loop of " + i);
-                }
-            }
-
+            business.main(i);
         }
+    }
+}
+
+class Business{
+    boolean bShouldSub = false;
+
+    public synchronized void sub(int i){
+        if(!bShouldSub){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int j = 0; j < 10; j++) {
+            System.out.println("sub thread sequece of " + j + ", loop of " + i);
+        }
+
+        bShouldSub = false;
+        this.notify();
+    }
+
+    public synchronized void main(int i){
+        if(bShouldSub){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int j = 0; j < 100; j++) {
+            System.out.println("main thread sequece of " + j + ", loop of " + i);
+        }
+
+        bShouldSub = true;
+        this.notify();
     }
 }
